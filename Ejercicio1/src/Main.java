314de16ecf21235900e9278725e60acf5c101911
bonelessnugget <yszeedyt99@gmail.com>
2022-01-10 12:27:38 +0100

    Ejercicio 1

---
+++ Ejercicio1/src/Main.java
@@ -0,0 +1,13 @@
+/*
+ * Realiza un conversor de euros a pesetas. La cantidad de euros que se quiere convertir debe ser
+introducida por teclado.
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		// 1 euro = 166,386 Pesetas
+		
+	}
+
+}
